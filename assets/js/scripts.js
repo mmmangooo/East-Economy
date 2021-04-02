940f9f31687de8792b1a2f9dc534980ebefa333a
mmmangooo <isabelle.angsas@gmail.com>
2021-04-02 20:46:35 +0000

    Added fade in effect on glass container

---
+++ assets/js/scripts.js
@@ -0,0 +1,28 @@
+/* Fade in glass container on page load, 
+   credit for fade effect code: 
+   https://www.geeksforgeeks.org/how-to-add-fade-in-effect-using-pure-javascript/ */
+
+var opacity = 0;
+var intervalId = 0;
+
+window.onload = showGlassContainer();
+
+function showGlassContainer() {
+
+    setInterval(fadeIn, 200)
+}
+
+
+function fadeIn() {
+      
+var glassContainer = document.getElementById('glass-container');
+opacity = Number(window.getComputedStyle(glassContainer).getPropertyValue('opacity'));
+
+if (opacity < 1) {
+opacity = opacity + 0.02;
+glassContainer.style.opacity = opacity;
+} else {
+    clearInterval(intervalId);
+}
+}
+
